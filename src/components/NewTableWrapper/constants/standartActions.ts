4fcf0e72dda721b34591b986437ed2cf4bca807c
Sergey Kolesnikov <trm@mail.ru>
2025-10-28 19:57:51 +0300

    main - little refac

--- src/components/NewTableWrapper/constants/standartActions.ts
+++ src/components/NewTableWrapper/constants/standartActions.ts
@@ -22,7 +22,6 @@ export const NEW_TABLE_STANDART_ROW_ACTIONS: Record<string, string> = {
   EXPAND_ON: 'expand-on',
   EXPAND_OFF: 'expand-off',
   CELL_ACTION: 'cell-action', // -> NEW_TABLE_STANDART_CELL_ACTIONS
-  CHANGE_CELL: 'change:cell'
 };
 
 export const NEW_TABLE_STANDART_CELL_ACTIONS: Record<string, string> = {
